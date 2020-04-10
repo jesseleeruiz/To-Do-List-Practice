57b9d487c37265844912c7b016b8820eb3c6fe60
jesseleeruiz <jesseleeruiz@gmail.com>
2020-04-10 11:21:55 -1000

    Added more to storyboard to build out skeleton of all features. Added Model File.

---
+++ Practice Part Two/Practice Part Two/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Practice Part Two
+//
+//  Created by Jesse Ruiz on 4/10/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import Foundation
+
+struct Model {
+    var text: String
+}
