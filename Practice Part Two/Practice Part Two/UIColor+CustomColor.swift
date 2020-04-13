227e92d65a93cff16bb9481d8817a9d9126677a4
jesseleeruiz <jesseleeruiz@gmail.com>
2020-04-13 12:10:57 -1000

    Added colors for gradients in UIColor+CustomColor file.

---
+++ Practice Part Two/Practice Part Two/UIColor+CustomColor.swift
@@ -0,0 +1,44 @@
+//
+//  UIColor+CustomColor.swift
+//  Practice Part Two
+//
+//  Created by Jesse Ruiz on 4/10/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let masterCard1 = UIColor(red: 244, green: 107, blue: 69, alpha: 1)
+    static let masterCard2 = UIColor(red: 238, green: 168, blue: 73, alpha: 1)
+    
+    static let clearSky1 = UIColor(red: 0, green: 92, blue: 151, alpha: 1)
+    static let clearSky2 = UIColor(red: 54, green: 55, blue: 149, alpha: 1)
+    
+    static let passion1 = UIColor(red: 229, green: 57, blue: 53, alpha: 1)
+    static let passion2 = UIColor(red: 227, green: 93, blue: 91, alpha: 1)
+    
+    static let timber1 = UIColor(red: 252, green: 0, blue: 255, alpha: 1)
+    static let timber2 = UIColor(red: 0, green: 219, blue: 222, alpha: 1)
+    
+    static let betweenNightAndDay1 = UIColor(red: 44, green: 62, blue: 80, alpha: 1)
+    static let betweenNightAndDay2 = UIColor(red: 52, green: 152, blue: 219, alpha: 1)
+    
+    static let virginAmerica1 = UIColor(red: 123, green: 67, blue: 151, alpha: 1)
+    static let virginAmerica2 = UIColor(red: 220, green: 36, blue: 48, alpha: 1)
+    
+    static let turquoiseFlow1 = UIColor(red: 19, green: 106, blue: 138, alpha: 1)
+    static let turquoiseFlow2 = UIColor(red: 38, green: 120, blue: 113, alpha: 1)
+    
+    static let endlessRiver1 = UIColor(red: 67, green: 206, blue: 162, alpha: 1)
+    static let endlessRiver2 = UIColor(red: 24, green: 90, blue: 157, alpha: 1)
+    
+    static let predawn1 = UIColor(red: 255, green: 161, blue: 127, alpha: 1)
+    static let predawn2 = UIColor(red: 0, green: 34, blue: 62, alpha: 1)
+    
+    static let shore1 = UIColor(red: 112, green: 225, blue: 245, alpha: 1)
+    static let shore2 = UIColor(red: 255, green: 209, blue: 148, alpha: 1)
+    
+    static let starfall1 = UIColor(red: 240, green: 194, blue: 123, alpha: 1)
+    static let starfall2 = UIColor(red: 75, green: 18, blue: 72, alpha: 1)
+}
